bc29e87ffd3a5c9ef57b681ee68454199a9323e5
Alexander Weitz <ajveits@gmail.com>
2023-05-19 08:24:06 +0900

    fixed fish configuration

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> /Users/$USER/.zprofile
 eval $(/opt/homebrew/bin/brew shellenv)
 echo "$(curl -fsSL https://raw.githubusercontent.com/ajweitz/profile/HEAD/personal_brews.txt)" | xargs -I {} brew install {}
 echo "$(curl -fsSL https://raw.githubusercontent.com/ajweitz/profile/HEAD/casks.txt)" | xargs -I {} brew install --cask {}
-echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
-chsh -s /usr/local/bin/fish
+# configure fish as default shell
+echo "$(which fish)" | sudo tee -a /etc/shells
+chsh -s $(which fish)
