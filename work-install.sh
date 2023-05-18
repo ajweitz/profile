22a14a44960723a5df598b489a753b21f55cd1b8
Alexander Weitz <ajveits@gmail.com>
2023-05-19 08:13:30 +0900

    added vscode install

--- work-install.sh
+++ work-install.sh
@@ -4,3 +4,4 @@
 echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> /Users/$USER/.zprofile
 eval $(/opt/homebrew/bin/brew shellenv)
 echo "$(curl -fsSL https://raw.githubusercontent.com/ajweitz/profile/HEAD/work-brews.txt)" | xargs -I {} brew install {}
+echo "$(curl -fsSL https://raw.githubusercontent.com/ajweitz/profile/HEAD/casks.txt)" | xargs -I {} brew install --cask {}
